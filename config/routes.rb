4d3451eef2d14a1f673954b51d266e929747ec2c
Diego Garces Jimenez <diegogarcesj1@gmail.com>
2021-08-03 18:26:42 -0400

    Routing and Controllers and ugly Views. TODO: Nice views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get "lists", to: "lists#index"
+  #get "lists/new", to: "lists#new"
+  #get "lists/:id", to: "lists#show", as: :list
+  #post "lists", to: "lists#create"
+  resources :lists, only: [ :index, :show, :new, :create ] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+  resources :bookmarks, only: [:destroy]
 end
