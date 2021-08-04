a2baa88c70decdc5afeab0e41dfa594f1385a261
Diego Garces Jimenez <diegogarcesj1@gmail.com>
2021-08-04 14:09:31 -0400

    BookmarksController refactored

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,12 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+  before_action :set_bookmark, only: [:destory]
+
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
@@ -16,13 +17,20 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
 
     redirect_to list_path(@bookmark.list)
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
