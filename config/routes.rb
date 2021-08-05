376fafd9cf6b175a2d038917eacd28a28471d7c3
Diego Garces Jimenez <diegogarcesj1@gmail.com>
2021-08-05 15:15:53 -0400

    Added root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   #get "lists/new", to: "lists#new"
   #get "lists/:id", to: "lists#show", as: :list
   #post "lists", to: "lists#create"
-  resources :lists, only: [ :index, :show, :new, :create ] do
+  root to: 'lists#index'
+  resources :lists, only: [:show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create ]
   end
   resources :bookmarks, only: [:destroy]
